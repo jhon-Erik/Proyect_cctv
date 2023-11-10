41f3b7d0c84b20c7b909dd8929de01b046bd9f53
jhon patiño <jhon@gmail.com>
2023-11-10 01:17:20 -0500

    creacion de los metodos, obtener, insertar, actualizar y eliminar

--- src/main/java/com/cctv/proyecto/app/controller/IncidenciaController.java
+++ src/main/java/com/cctv/proyecto/app/controller/IncidenciaController.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +36,28 @@ public class IncidenciaController {
 		return incidenciaService.listar();
 	}
 
+    
+    
+    
+   // public ResponseEntity<Incidencia>obtenerIncidencia(@PathVariable(value="8095")Integer id){
+    //	try {
+    	//	return ResponseEntity.status(HttpStatus.OK).body(incidenciaService.ObtenerIncidencia(id));
+			
+//		} catch (Exception e) {
+			
+	//	} 
+    //}
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED, reason = "se creó correctamente")
     @ResponseBody
@@ -43,5 +68,31 @@ public class IncidenciaController {
     	}else {
     		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     	}
+    }
+    
+    
+    @PutMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.ACCEPTED, reason = "se actulizó correctamente")
+    @ResponseBody
+    public ResponseEntity<Incidencia>ActualizarIncidencia(@RequestBody Incidencia incidencia, @PathVariable(value="id")Integer id){
+    	incidencia =  incidenciaService.ActualizarIncidencia(incidencia);
+    	Incidencia incid  =  incidenciaService.ObtenerIncidencia(id);
+    	if(incidencia != null) {
+    		incidenciaService.ActualizarIncidencia(incid);
+    		return new ResponseEntity<>(incid,HttpStatus.ACCEPTED);
+    	}else {
+    		return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    	}
+    	
+    }
+    
+    
+    
+    
+    
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Incidencia EliminarIncidencia(@PathVariable(value="id")Integer id) {
+    	return incidenciaService.EliminarIncidencia(id);
     }
 }
